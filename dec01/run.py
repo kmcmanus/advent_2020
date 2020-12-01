35b10d3704b3fe2767bbb8ec15edbeac349247ab
Kevin McManus <kmcmanus98035@gmail.com>
2020-12-01 10:11:17 -0500

    more generic

--- dec01/run.py
+++ dec01/run.py
@@ -1,3 +1,21 @@
+from functools import reduce
+from itertools import combinations
+
+# A worse combinations
+def chain(data, layers):
+    for index, datum in enumerate(data):
+        if layers == 1:
+            yield [datum]
+        else:
+            for datas in chain(data[index+1:], layers - 1):
+                yield datas + [datum]
+
+def combine(data, layers):
+    return [
+      reduce(lambda x, y: x * y, bits, 1)
+      for bits in combinations(data, layers)
+      if sum(bits) == 2020
+    ][0]
 
 def one(data):
     for i, d in enumerate(data):
@@ -16,4 +34,6 @@ def two(data):
 with open('input', 'r') as f:
     data = [int(d.strip()) for d in f.readlines()]
 print(one(data))
+print(combine(data, 2))
 print(two(data))
+print(combine(data, 3))
