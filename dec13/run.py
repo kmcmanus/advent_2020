ec247125d12b481f4259bf7e9f7f72dfc2fa68ec
Kevin McManus <kmcmanus98035@gmail.com>
2020-12-13 10:20:41 -0500

    Part 2

--- dec13/run.py
+++ dec13/run.py
@@ -1,5 +1,8 @@
 import math
 import functools
+import itertools
+from signal import signal, SIGINT
+from sys import exit
 
 with open('input', 'r') as f:
     time = int(f.readline().strip())
@@ -25,3 +28,47 @@ desired = functools.reduce(selection, avail.items())
 (bus, arrival_time) = desired
 print("one")
 print(bus * (arrival_time - time))
+
+def make_generator(shift, step, start):
+    start = math.ceil(start / step) * step
+    return (
+        i - shift
+        for i in itertools.count(start, step=step)
+    )
+start_at = 100000000000000
+with open('input', 'r') as f:
+    f.readline()
+    busses = [
+        make_generator(i, int(line.strip()), start_at)
+        for i, line in enumerate(f.readline().split(","))
+        if 'x' not in line
+    ]
+
+print(busses)
+
+lmax = 0
+def handler(signal_received, frame):
+    global lmax
+    print('SIGINT or CTRL-C detected. Exiting gracefully')
+    print(lmax)
+    exit(0)
+
+signal(SIGINT, handler)
+def intersection(left, right):
+    global lmax
+    l_val = left.__next__()
+    r_val = right.__next__()
+    while True:
+        while l_val < r_val:
+            l_val = left.__next__()
+        while r_val < l_val:
+            r_val = right.__next__()
+        if l_val == r_val:
+            lmax = max(lmax, l_val)
+            yield l_val
+            l_val = left.__next__()
+            r_val = right.__next__()
+
+final = functools.reduce(intersection, busses)
+print("two")
+print(final.__next__())
